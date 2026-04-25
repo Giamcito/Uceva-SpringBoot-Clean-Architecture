8eb5fd5a824e86fcae66845782ca1469dfda3e7e
Giamcito <juan.giraldo03@uceva.edu.co>
2026-04-25 04:03:04 -0500

    Registro de beans de orders a la configuracion

--- src/main/java/uceva/cleanarchitecture/config/AppConfig.java
+++ src/main/java/uceva/cleanarchitecture/config/AppConfig.java
@@ -4,12 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uceva.cleanarchitecture.application.usecase.GetAllProductsUseCase;
 import uceva.cleanarchitecture.application.usecase.GetAllUsersUseCase;
+import uceva.cleanarchitecture.application.usecase.GetAllOrdersUseCase;
 import uceva.cleanarchitecture.domain.repository.ProductRepository;
 import uceva.cleanarchitecture.domain.repository.UserRepository;
+import uceva.cleanarchitecture.domain.repository.OrderRepository;
 import uceva.cleanarchitecture.infrastructure.datasource.ProductDatasource;
 import uceva.cleanarchitecture.infrastructure.datasource.UserDatasource;
+import uceva.cleanarchitecture.infrastructure.datasource.OrderDatasource;
 import uceva.cleanarchitecture.infrastructure.repository.ProductRepositoryImpl;
 import uceva.cleanarchitecture.infrastructure.repository.UserRepositoryImpl;
+import uceva.cleanarchitecture.infrastructure.repository.OrderRepositoryImpl;
 
 @Configuration
 public class AppConfig {
@@ -25,6 +29,11 @@ public class AppConfig {
     }
 
     @Bean
+    OrderDatasource orderDatasource() {
+        return new OrderDatasource();
+    }
+
+    @Bean
     UserRepository userRepository(UserDatasource userDatasource) {
         return new UserRepositoryImpl(userDatasource);
     }
@@ -35,6 +44,11 @@ public class AppConfig {
     }
 
     @Bean
+    OrderRepository orderRepository(OrderDatasource orderDatasource) {
+        return new OrderRepositoryImpl(orderDatasource);
+    }
+
+    @Bean
     GetAllUsersUseCase getAllUsersUseCase(UserRepository userRepository) {
         return new GetAllUsersUseCase(userRepository);
     }
@@ -42,6 +56,11 @@ public class AppConfig {
     @Bean
     GetAllProductsUseCase getAllProductsUseCase(ProductRepository productRepository) {
         return new GetAllProductsUseCase(productRepository);
+    }
+
+    @Bean
+    GetAllOrdersUseCase getAllOrdersUseCase(OrderRepository orderRepository) {
+        return new GetAllOrdersUseCase(orderRepository);
     }
 
 }
